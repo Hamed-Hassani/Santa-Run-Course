a9cfdb8633804520c0aec919fcbe7a9f76f4c10a
HamedHassani <hamed2008ir@gmail.com>
2022-05-29 16:28:41 +0430

    upload game

---
+++ Assets/Scripts/ObstcaleSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstcaleSpawner : MonoBehaviour
+{
+    // public GameObject obstcale;
+    public static ObstcaleSpawner instance;
+    public GameObject[] obstacles;
+
+    public bool gameOver = false;
+
+    public float minSpawnTime, maxSpawnTime;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+    IEnumerator Spawn()
+    {
+        float waitTime = 1f;
+        yield return new WaitForSeconds(waitTime);
+
+        while (!gameOver)
+        {
+            SpawnObstcale();
+            waitTime = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+
+
+    void SpawnObstcale()
+    {
+        int random = Random.Range(0, obstacles.Length);
+        Instantiate(obstacles[random], transform.position, Quaternion.identity); 
+    }
+
+}
